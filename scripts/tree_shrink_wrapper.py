b2d0e066bf3179a817066f8a5b6dee38b005d097
diego <dfmoralesb@gmail.com>
2019-10-11 15:46:39 -0500

    New tree_shrink_wrapper compatible with TS 1.3.2

--- scripts/tree_shrink_wrapper.py
+++ scripts/tree_shrink_wrapper.py
@@ -1,43 +1,62 @@
 """
-Detects and trim abnormally long branches using TreeShrink
+Detects and trim abnormally long branches using TreeShrink v1.3.2
 
 TreeShrink must be installed and on path
 Phyx (pxrr) must be installed and on path
 
 """
 
+
 import sys, os, shutil
 
-def trim(DIR,tree_file_ending,q):
+def trim(inDIR,tree_file_ending,q,outDIR):
 
-	if os.path.isabs(DIR) == False: DIR = os.path.abspath(DIR)
-	if DIR[-1] != "/": DIR += "/"
+	if os.path.isabs(inDIR) == False: inDIR = os.path.abspath(inDIR)
+	if inDIR[-1] != "/": inDIR += "/"
+	if os.path.isabs(outDIR) == False: outDIR = os.path.abspath(outDIR)
+	if outDIR[-1] != "/": outDIR += "/"
+	if outDIR == ".": outDIR = os.getcwd()
+	
 	
 	filecount = 0
 	
 	#runs treeshrink
-	for i in os.listdir(DIR):
+	for i in os.listdir(inDIR):
 		if i.endswith(tree_file_ending):
 			print i
 			filecount += 1
-			cmd= ["treeshrink.py","-i", DIR+i ,"-o", i+".ts", "-c","-m per-gene", "-q"+str(q), "-d",DIR+i+".ts_dir"]
+			cmd= ["run_treeshrink.py","-t", inDIR+i ,"-c","-m per-gene", "-q "+str(q), "-o",outDIR+i+".ts_dir"]
 			print (" ".join(cmd))
 			os.system(" ".join(cmd))
+			
+			
+	#go into each TS folder and change extension from 'tree_file_ending' to ts
+	
+	for f in os.listdir(outDIR):
+		if f.endswith(".ts_dir"):
+			for t in os.listdir(outDIR+f):
+				if t.endswith(tree_file_ending):
+					if f[-1] != "/": f += "/"			
+					basename = os.path.splitext(outDIR+f+t)[0]
+					os.rename(outDIR+f+t, basename + ".ts")
+					
+					
 	
 	#moves output files to DIR and delete treeshrink individual folders
-	for j in os.listdir(DIR):
+	for j in os.listdir(outDIR):
 		if j.endswith(".ts_dir"):
-			source = DIR+j
-			dest = DIR
+			source = outDIR+j
+			dest = outDIR
 			files = os.listdir(source)
 			for f in files:
 				shutil.move(source+"/"+f, dest)
-			shutil.rmtree(DIR+j)
+			shutil.rmtree(outDIR+j)
+	
 	
 	#removes single quotes from tip labels from treeshrink output trees
-	for k in os.listdir(DIR):
+	for k in os.listdir(outDIR):
 		if k.endswith(".ts"):
-			with open(DIR+k, 'r+') as f:
+			with open(outDIR+k, 'r+') as f:
 				content = f.read()
 				f.seek(0)
 				f.truncate()
@@ -45,28 +64,26 @@ def trim(DIR,tree_file_ending,q):
 			f.close()
 	
 	#unroot treeshrink ouput trees
-	for l in os.listdir(DIR):
+	for l in os.listdir(outDIR):
 		if l.endswith(".ts"):
-			cmd= ["pxrr","-u","-t", DIR+l,"-o",DIR+l+".tt"]
+			cmd= ["pxrr","-u","-t", outDIR+l,"-o",outDIR+l+".tt"]
 			print (" ".join(cmd))
 			os.system(" ".join(cmd))			
 			
 	#delete ts files
-	for m in os.listdir(DIR):
+	for m in os.listdir(outDIR):
 			if m.endswith(".ts"):
-    				os.remove(DIR+m)
-            
+    				os.remove(outDIR+m)
+        
             
 	assert filecount > 0, \
-		"No file end with "+tree_file_ending+" found in "+DIR
+		"No file end with "+tree_file_ending+" found in "+inDIR
 			
 			
 if __name__ == "__main__":
-	if len(sys.argv) != 4:
-		print "python tree_shrink_wrapper.py DIR tree_file_ending quantile"
+	if len(sys.argv) != 5:
+		print "python tree_shrink_wrapper.py inDIR tree_file_ending quantile outDIR"
 		sys.exit(0)
 
-	DIR,tree_file_ending,q = sys.argv[1:]
-	trim(DIR,tree_file_ending,q)
-
-
+	inDIR,tree_file_ending,q,outDIR = sys.argv[1:]
+	trim(inDIR,tree_file_ending,q,outDIR)
